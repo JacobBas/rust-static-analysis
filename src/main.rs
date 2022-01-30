23b5bb52c0b1c327891aeb82568f342b35f65721
JacobBas <basjacob17@gmail.com>
2022-01-29 17:46:20 -0700

    just using the build tree-sitter-python package instead of building from source

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// #[test]
+// fn test_parser() {
+//     let language = unsafe {tree_sitter_python()};
+//     let mut parser = Parser::new();
+//     parser.set_language(language).unwrap();
+
+//     let source_code = "print('hello there')";
+//     let tree = parser.parse(source_code, None).unwrap();
+
+//     assert_eq!(tree.root_node().to_sexp(), "(source_file ())")
+// }
